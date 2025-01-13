f3cdcf5e8dcad63f45ef19e1e59afee02d29b506
AndersonFBD <andersonf.barrosdias@gmail.com>
2025-01-13 11:52:51 -0300

    feat: add user schema model

---
+++ models/UserModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  admin: Boolean,
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+
+module.exports = {
+  list: async () => {
+    const userList = await UserModel.find({});
+    return userList;
+  },
+  save: async (username, password, admin) => {
+    const newUser = new UserModel({
+      username: username,
+      password: password,
+      admin: admin,
+    });
+    await newUser.save();
+    return newUser;
+  },
+  update: async (id, editedUser) => {
+    let foundUser = await UserModel.findById(id);
+    if (!foundUser) return false;
+
+    Object.keys(editedUser).forEach(
+      (key) => (foundUser[key] = editedUser[key])
+    );
+    await foundUser.save();
+    return foundUser;
+  },
+  delete: async (id) => {
+    return await UserModel.findByIdAndDelete(id);
+  },
+  getUser: async (id) => {
+    return await UserModel.findById(id);
+  },
+};
